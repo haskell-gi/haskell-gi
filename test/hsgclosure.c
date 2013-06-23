f6186f2b4c665223d3d5a35d6d38f2015ae03c9b
Iñaki García Etxebarria <garetxe@gmail.com>
2013-06-23 17:56:16 +0200

    Better error reporting the castTo fails

--- test/hsgclosure.c
+++ test/hsgclosure.c
@@ -293,7 +293,23 @@ gtk2hs_value_from_haskellobj(GValue *value, HaskellObj obj) {
 
 int check_object_type(void *instance, GType type)
 {
-  return !!G_TYPE_CHECK_INSTANCE_TYPE(instance, type);
+  int result;
+
+  if (instance != NULL) {
+     result = !!G_TYPE_CHECK_INSTANCE_TYPE(instance, type);
+
+     if (result == 0) {
+       GType instance_type = G_TYPE_FROM_INSTANCE (instance);
+       fprintf(stderr, "Check failed: %s is not a %s\n",
+	       g_type_name (instance_type),
+	       g_type_name (type));
+     }
+  } else {
+    result = 0;
+    fprintf(stderr, "Check failed: got a null pointer\n");
+  }
+
+  return result;
 }
 
 /* Auxiliary function for freeing boxed types */
