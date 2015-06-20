cba0eb2be8f45a3179e322eb1dd0aad167e72b3e
Iñaki García Etxebarria <garetxe@gmail.com>
2015-06-21 00:54:33 +0200

    Remove autogenerated Setup.hs files

--- bindings/GdkPixbuf/Setup.hs
+++
@@ -1,7 +0,0 @@
-import Distribution.Simple
-
-main = defaultMainWithHooks simpleUserHooks {
-         haddockHook = haddockHook
-       }
-    where
-      haddockHook _ _ _ _ = putStrLn "Skipping haddock, this package provides no documentation."
