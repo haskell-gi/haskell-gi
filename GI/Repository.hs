0b225a281c1b0866de11dd66cad9453543f06162
Jonas Platte <mail@jonasplatte.de>
2015-07-09 17:42:32 +0200

    Improved readability of GI/Repository.hs

--- GI/Repository.hs
+++ GI/Repository.hs
@@ -1,13 +1,13 @@
+{-# LANGUAGE LambdaCase #-}
+
 module GI.Repository
     ( readGiRepository
     ) where
 
 import Prelude hiding (readFile)
 
-import Control.Applicative ((<|>))
-import Control.Monad (liftM, when)
+import Control.Monad (when)
 import qualified Data.List as List
-import Data.Bool (bool)
 import Data.Maybe
 import Safe (maximumMay)
 import Text.XML
@@ -19,33 +19,41 @@ import System.FilePath
 girDataDirs :: IO [FilePath]
 girDataDirs = getSystemDataDirs "gir-1.0"
 
-girFile' :: String -> Maybe String -> FilePath -> IO (Maybe FilePath)
-girFile' name version path = do
-    dirExists <- doesDirectoryExist path
-    if dirExists then do
-        repositories <- liftM takeBaseName <$> getDirectoryContents path
-        let maxAvailVersion = maximumMay . catMaybes $
-                List.stripPrefix (name ++ "-") <$> repositories
-            filePath = (path </>) . (name ++) . ("-" ++) . (<.> "gir")
-                <$> (version <|> maxAvailVersion)
+girFilePath :: String -> String -> FilePath -> FilePath
+girFilePath name version path = path </> name ++ "-" ++ version <.> "gir"
 
-        doesFileExist (fromMaybe "" filePath) >>= bool
-            (return Nothing)
-            (return filePath)
-    else
-        return Nothing
+girFile' :: String -> Maybe String -> FilePath -> IO (Maybe FilePath)
+girFile' name (Just version) path =
+    let filePath = girFilePath name version path
+    in  doesFileExist filePath >>= \case
+        True  -> return $ Just filePath
+        False -> return Nothing
+girFile' name Nothing path =
+    doesDirectoryExist path >>= \case
+        True -> do
+            repositories <- map takeBaseName <$> getDirectoryContents path
+            let version = maximumMay . catMaybes $
+                    List.stripPrefix (name ++ "-") <$> repositories
+
+            return $ case version of
+                Just v  -> Just $ girFilePath name v path
+                Nothing -> Nothing
+
+        False -> return Nothing
 
 girFile :: String -> Maybe String -> IO (Maybe FilePath)
-girFile name version = listToMaybe <$> catMaybes <$>
-    (girDataDirs >>= mapM (girFile' name version))
+girFile name version =
+    firstJust <$> (girDataDirs >>= mapM (girFile' name version))
+    where firstJust = listToMaybe . catMaybes
 
 readGiRepository :: Bool -> String -> Maybe String -> IO Document
 readGiRepository verbose name version =
-    girFile name version >>= maybe
-        (do dataDirs <- girDataDirs
+    girFile name version >>= \case
+        Just path -> do
+            when verbose $ putStrLn $ "Loading GI repository: " ++ path
+            readFile def path
+        Nothing -> do
+            dataDirs <- girDataDirs
             error $ "Did not find a GI repository for " ++ name
                 ++ maybe "" ("-" ++) version
-                ++ " in " ++ show dataDirs)
-        (\path -> do
-            when verbose $ putStrLn $ "Loading GI repository: " ++ path
-            readFile def path)
+                ++ " in " ++ show dataDirs
