93bbbd1b1a1d43198f97fa79ae38f59dd9d96819
Iñaki García Etxebarria <garetxe@gmail.com>
2015-11-01 17:37:26 +0100

    Explicit reexports
    Reexport modules explicitly, so we get better haddocks.

--- src/Data/GI/Base.hs
+++ src/Data/GI/Base.hs
@@ -1,14 +1,35 @@
-module Data.GI.Base (module X) where
-import Data.GI.Base.Attributes       as X
-import Data.GI.Base.BasicConversions as X
-import Data.GI.Base.BasicTypes       as X
-import Data.GI.Base.Closure          as X
-import Data.GI.Base.GError           as X
-import Data.GI.Base.GHashTable       as X
-import Data.GI.Base.GValue           as X
-import Data.GI.Base.GVariant         as X
-import Data.GI.Base.GParamSpec       as X
-import Data.GI.Base.ManagedPtr       as X
-import Data.GI.Base.Properties       as X
-import Data.GI.Base.Signals          as X
-import Data.GI.Base.Utils            as X
+{- |
+   == Convenience header for basic GObject-Introspection modules
+
+See the documentation for each individual module for a description and
+usage help.
+-}
+module Data.GI.Base
+    ( module Data.GI.Base.Attributes
+    , module Data.GI.Base.BasicConversions
+    , module Data.GI.Base.BasicTypes
+    , module Data.GI.Base.Closure
+    , module Data.GI.Base.GError
+    , module Data.GI.Base.GHashTable
+    , module Data.GI.Base.GValue
+    , module Data.GI.Base.GVariant
+    , module Data.GI.Base.GParamSpec
+    , module Data.GI.Base.ManagedPtr
+    , module Data.GI.Base.Properties
+    , module Data.GI.Base.Signals
+    , module Data.GI.Base.Utils
+    ) where
+
+import Data.GI.Base.Attributes
+import Data.GI.Base.BasicConversions
+import Data.GI.Base.BasicTypes
+import Data.GI.Base.Closure
+import Data.GI.Base.GError
+import Data.GI.Base.GHashTable
+import Data.GI.Base.GValue
+import Data.GI.Base.GVariant
+import Data.GI.Base.GParamSpec
+import Data.GI.Base.ManagedPtr
+import Data.GI.Base.Properties
+import Data.GI.Base.Signals
+import Data.GI.Base.Utils
