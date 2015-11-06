b9c330622ac0cc6a7fceefafaa0f2f82f237da98
Jonas Platte <mail@jonasplatte.de>
2015-11-06 13:59:27 +0100

    Initial commit

---
+++ src/Data/GI/Gtk.hs
@@ -0,0 +1,7 @@
+module Data.GI.Gtk
+    ( module GI.Gtk
+    , module Data.GI.Gtk.BuildFn
+    ) where
+
+import GI.Gtk
+import Data.GI.Gtk.BuildFn
---
+++ src/Data/GI/Gtk/BuildFn.hs
@@ -0,0 +1,57 @@
+{-|
+A module aimed at making working with GtkBuilder easier.
+It's meant to be used like this (requires OverloadedStrings):
+
+> buildUI :: BuildFn ()
+> buildUI = do
+>     mainWin <- buildMainWin
+>     buildAboutDialog
+>
+>     widgetShowAll mainWin
+>
+> buildMainWin :: BuildFn ApplicationWindow
+> buildMainWin = do
+>     buildHeaderBar
+>     buildMenuBar
+>     buildMainArea
+>
+>     mainWin <- getObject' ApplicationWindow "mainWin"
+>     on mainWin Destroy mainQuit
+>     return mainWin
+>
+> buildAboutDialog :: BuildFn AboutDialog
+> ...
+>
+> buildHeaderBar :: BuildFn HeaderBar
+> ...
+>
+> buildMenuBar :: BuildFn MenuBar
+> ...
+>
+> buildMainArea :: BuildFn Grid
+> ...
+-}
+module Data.GI.Gtk.BuildFn
+    ( BuildFn
+    , buildWithBuilder
+    , getObject
+    ) where
+
+import           Control.Monad.Reader (ReaderT, runReaderT, ask, MonadIO, liftIO)
+import           Data.GI.Base (GObject, castTo)
+import           Data.Maybe (fromJust)
+import qualified Data.Text as T
+import           Foreign.ForeignPtr (ForeignPtr)
+
+import           GI.Gtk hiding (main)
+
+type BuildFn a = ReaderT Builder IO a
+
+buildWithBuilder :: MonadIO m => BuildFn a -> Builder -> m a
+buildWithBuilder fn builder = liftIO $ runReaderT fn builder
+
+getObject :: GObject a => (ForeignPtr a -> a) -> T.Text -> BuildFn a
+getObject ctor name = do
+    builder <- ask
+    obj <- builderGetObject builder name
+    liftIO $ fromJust <$> castTo ctor obj
