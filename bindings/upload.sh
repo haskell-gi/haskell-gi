88b914d692ffe5a6ca7cf9ddfb5896414facfa44
Iñaki García Etxebarria <garetxe@gmail.com>
2016-03-25 21:04:06 +0100

    Skip already uploaded packages

--- bindings/upload.sh
+++ bindings/upload.sh
@@ -8,7 +8,13 @@ for sdist in sdists/*; do
         echo "OK"
         rm $sdist
     else
-        echo "Failed to upload $sdist: $result"
-        exit 1
+        echo $result | grep -q "already been uploaded"
+        if [ $? == 0 ]; then
+            echo "Already uploaded, OK"
+            rm $sdist
+        else
+            echo "Failed to upload $sdist: $result"
+            exit 1
+        fi
     fi
 done
