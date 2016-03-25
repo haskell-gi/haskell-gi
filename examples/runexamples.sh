1a4681997ebef743a62c37ced90d9dab33f7c32e
Iñaki García Etxebarria <garetxe@gmail.com>
2016-03-25 21:05:08 +0100

    Generate overloaded labels for GStreamer libs

--- examples/runexamples.sh
+++ examples/runexamples.sh
@@ -4,8 +4,8 @@ set -e
 set -x
 
 cabal clean
-haskell-gi -l Gtk WebKit2
-haskell-gi -c Gtk WebKit2
+haskell-gi -l Gtk WebKit2 Gst GstBase GstAudio GstVideo
+haskell-gi -c Gtk WebKit2 Gst GstBase GstAudio GstVideo
 cabal configure
 time cabal build
 
