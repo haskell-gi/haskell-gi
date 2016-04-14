a8a3cf8bdd448f8755932d43c1036309b7106153
Jonas Platte <mail@jonasplatte.de>
2016-04-14 14:34:37 +0200

    Revert terse reexport to prevent empty haddock docs for Data.GI.Gtk

--- src/Data/GI/Gtk.hs
+++ src/Data/GI/Gtk.hs
@@ -1,16 +1,20 @@
 module Data.GI.Gtk
-    ( module Export
+    ( module GI.Gtk
+    , module Data.GI.Gtk.ModelView.Types
+    , module Data.GI.Gtk.ModelView.CellLayout
+    , module Data.GI.Gtk.ModelView.CustomStore
+    , module Data.GI.Gtk.ModelView.SeqStore
+    , module Data.GI.Gtk.ModelView.ForestStore
+    , module Data.GI.Gtk.ModelView.TreeModel
+    , module Data.GI.Gtk.ComboBox
     ) where
 
-import GI.Gtk as Export hiding
-    ( treeModelGetValue
-    , treeModelGetIter
-    )
-import Data.GI.Gtk.BuildFn               as Export
-import Data.GI.Gtk.ModelView.Types       as Export
-import Data.GI.Gtk.ModelView.CellLayout  as Export
-import Data.GI.Gtk.ModelView.CustomStore as Export
-import Data.GI.Gtk.ModelView.SeqStore    as Export
-import Data.GI.Gtk.ModelView.ForestStore as Export
-import Data.GI.Gtk.ModelView.TreeModel   as Export
-import Data.GI.Gtk.ComboBox              as Export
+import GI.Gtk hiding (treeModelGetValue, treeModelGetIter)
+import Data.GI.Gtk.BuildFn
+import Data.GI.Gtk.ModelView.Types
+import Data.GI.Gtk.ModelView.CellLayout
+import Data.GI.Gtk.ModelView.CustomStore
+import Data.GI.Gtk.ModelView.SeqStore
+import Data.GI.Gtk.ModelView.ForestStore
+import Data.GI.Gtk.ModelView.TreeModel
+import Data.GI.Gtk.ComboBox
