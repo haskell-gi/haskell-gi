d3de5acfa8ee55e2594416fb107663ffb8a2573c
Iñaki García Etxebarria <garetxe@gmail.com>
2016-04-26 10:44:06 +0200

    Add HASKELL_GI_GIR_SEARCH_PATH
    This is a list of colon-separated paths which will be prepended to the
    `.gir` file search path.

--- src/GI/GIR/Repository.hs
+++ src/GI/GIR/Repository.hs
@@ -12,9 +12,10 @@ import qualified Data.List as List
 import qualified Data.Text as T
 import Data.Text (Text)
 import Safe (maximumMay)
-import Text.XML
+import qualified Text.XML as XML
 
 import System.Directory
+import System.Environment (lookupEnv)
 import System.Environment.XDG.BaseDir (getSystemDataDirs)
 import System.FilePath
 
@@ -43,18 +44,41 @@ girFile' name Nothing path =
 
         False -> return Nothing
 
+-- | Split a list into sublists delimited by the given element.
+splitOn :: Eq a => a -> [a] -> [[a]]
+splitOn x xs = go xs []
+    where go [] acc = [reverse acc]
+          go (y : ys) acc = if x == y
+                            then reverse acc : go ys []
+                            else go ys (y : acc)
+
+-- | Search for an appropriate @.gir@ file in the search path. This is
+-- either passed in explicitly, or if that is absent, loaded from the
+-- environment variable @HASKELL_GI_GIR_SEARCH_PATH@. In either case
+-- the system data dirs are also searched if nothing can be found in
+-- the explicitly passed paths, or in the contents of
+-- @HASKELL_GI_GIR_SEARCH_PATH@.
 girFile :: Text -> Maybe Text -> [FilePath] -> IO (Maybe FilePath)
+girFile name version [] =
+    lookupEnv "HASKELL_GI_GIR_SEARCH_PATH" >>= \case
+      Nothing -> girFile name version []
+      Just s -> girFile name version (splitOn ':' s)
 girFile name version extraPaths = do
   dataDirs <- girDataDirs
   firstJust <$> (mapM (girFile' name version) (extraPaths ++ dataDirs))
     where firstJust = listToMaybe . catMaybes
 
-readGiRepository :: Bool -> Text -> Maybe Text -> [FilePath] -> IO Document
+-- | Try to load the `.gir` file corresponding to the given repository
+readGiRepository :: Bool        -- ^ verbose
+                 -> Text        -- ^ name
+                 -> Maybe Text  -- ^ version
+                 -> [FilePath]  -- ^ searchPath
+                 -> IO XML.Document
 readGiRepository verbose name version extraPaths =
     girFile name version extraPaths >>= \case
         Just path -> do
             when verbose $ putStrLn $ "Loading GI repository: " ++ path
-            readFile def path
+            XML.readFile XML.def path
         Nothing -> do
             dataDirs <- girDataDirs
             error $ "Did not find a GI repository for " ++ (T.unpack name)
