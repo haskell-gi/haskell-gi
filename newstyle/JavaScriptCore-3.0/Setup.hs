02e13e27e318aebfdfc238ba51948abf19b8fd22
Iñaki García Etxebarria <garetxe@gmail.com>
2016-05-18 21:46:25 +0200

    Port a few more bindings to the new style

---
+++ newstyle/JavaScriptCore-3.0/Setup.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Distribution.Simple (defaultMainWithHooks)
+import Data.GI.CodeGen.CabalHooks (simpleHaskellGIHooks)
+
+main = defaultMainWithHooks (simpleHaskellGIHooks name version verbose
+                             overridesFile outputDir)
+    where name = "JavaScriptCore"
+          version = "3.0"
+          verbose = False
+          overridesFile = Nothing
+          outputDir = Nothing
