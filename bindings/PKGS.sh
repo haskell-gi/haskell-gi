b9ed7c34778047890e09e945a8ec030f5ab28610
Iñaki García Etxebarria <garetxe@gmail.com>
2016-06-05 23:04:18 +0200

    Compile Soup after Gtk

--- bindings/PKGS.sh
+++ bindings/PKGS.sh
@@ -3,7 +3,7 @@ basever=$(ghc-pkg list base)
 unamestr=$(uname)
 
 # Packages compatible with base >= 4.7
-base47PKGS="GLib GObject Atk Gio Soup cairo Pango PangoCairo GdkPixbuf Gdk Gtk GtkSource GIRepository Poppler JavaScriptCore-3.0 WebKit Vte Notify"
+base47PKGS="GLib GObject Atk Gio cairo Pango PangoCairo GdkPixbuf Gdk Gtk GtkSource Soup GIRepository Poppler JavaScriptCore-3.0 WebKit Vte Notify"
 # Packages compatible with base >= 4.8
 base48PKGS="Gst GstBase GstAudio GstVideo"
 
