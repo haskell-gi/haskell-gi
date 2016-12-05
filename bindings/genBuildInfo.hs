e15cf8faeee3ee48091fcaaea8f88e95b256f269
Iñaki García Etxebarria <garetxe@gmail.com>
2016-12-05 22:31:29 +0100

    Support for stack

--- bindings/genBuildInfo.hs
+++ bindings/genBuildInfo.hs
@@ -7,8 +7,11 @@
 
 import System.Environment (getArgs)
 import System.FilePath ((</>), (<.>))
+import System.Console.GetOpt
+import System.IO (hPutStr, hPutStrLn, stderr)
+import System.Exit (exitFailure)
 
-import Control.Monad (forM_)
+import Control.Monad (forM_, when)
 import GHC.Generics
 
 import qualified Data.Aeson as A
@@ -25,6 +28,13 @@ import ProjectInfo (ProjectInfo(..))
 
 import qualified Data.ByteString.Lazy as LB
 
+-- | Which kind of project to generate. Unfortunately these are not
+-- mutually compatible due to limitations in stack... see
+-- https://github.com/commercialhaskell/stack/issues/2094
+data ProjectType = CabalNewBuild
+                 | Stack
+                   deriving (Eq, Show)
+
 readGIRInfo :: FilePath -> IO ProjectInfo
 readGIRInfo fname = do
   buf <- LB.readFile fname
@@ -32,9 +42,9 @@ readGIRInfo fname = do
     Left err -> error ("Could not parse \"" <> fname <> "\": " <> err)
     Right info -> return info
 
-writeCabal :: FilePath -> ProjectInfo -> IO ()
-writeCabal fname info =
-    B.writeFile fname $ TE.encodeUtf8 $ T.unlines
+writeCabal :: ProjectType -> FilePath -> ProjectInfo -> IO ()
+writeCabal pType fname info =
+    B.writeFile fname $ TE.encodeUtf8 $ T.unlines $
        [ "name:                 " <> name info
        , "version:              " <> version info
        , "synopsis:             " <> synopsis info
@@ -46,16 +56,24 @@ writeCabal fname info =
        , "maintainer:           " <> PI.maintainers
        , "category:             " <> PI.category
        , "build-type:           Custom"
-       , "cabal-version:        >= 1.24"
-       , case girOverrides info of
-           Nothing -> ""
-           Just ov -> "\nextra-source-files: " <> ov <> "\n"
-       , "custom-setup"
-       , "      setup-depends: base >= 4.7 && < 5,"
-       , "                     Cabal >= 1.24,"
-       , "                     haskell-gi >= 0.20 && < 1"
-       , ""
-       , "Flag overloaded-methods"
+       ] <>
+       (if pType == CabalNewBuild
+       then [ "cabal-version:        >= 1.24"
+             , case girOverrides info of
+                Nothing -> ""
+                Just ov -> "\nextra-source-files: " <> ov <> "\n"
+            , "custom-setup"
+            , "      setup-depends: base >= 4.7 && < 5,"
+            , "                     Cabal >= 1.24,"
+            , "                     haskell-gi >= 0.20 && < 1"
+            , "" ]
+       else [ "cabal-version:        >= 1.22"
+            , case girOverrides info of
+                Nothing -> "\nextra-source-files: stack.yaml\n"
+                Just ov -> "\nextra-source-files: stack.yaml " <> ov <> "\n"
+            ])
+       <>
+       [ "Flag overloaded-methods"
        , "      Description: Generate support for overloaded methods."
        , ""
        , "Flag overloaded-properties"
@@ -73,9 +91,12 @@ writeCabal fname info =
        , "      pkgconfig-depends: " <> pkgconfigDepends info
        , "      build-depends: " <>
          T.intercalate ",\n                     "
-              ([baseVersion info
-               , "haskell-gi-base >= 0.20 && < 1"
-               ] ++ giDepends info ++ PI.standardDeps)
+              ([baseVersion info] <>
+               (if pType == CabalNewBuild
+                then ["haskell-gi-base >= 0.20 && < 1"]
+                else ["haskell-gi-base >= 0.20 && < 1"
+                     ,"haskell-gi >= 0.20 && < 1"])
+               ++ giDepends info ++ PI.standardDeps)
        ]
 
 writeSetup :: FilePath -> ProjectInfo -> IO ()
@@ -99,11 +120,57 @@ writeSetup fname info =
 writeLicense :: FilePath -> IO ()
 writeLicense fname = B.writeFile fname (TE.encodeUtf8 PI.licenseText)
 
+writeStackYaml :: FilePath -> IO ()
+writeStackYaml fname =
+    B.writeFile fname $ TE.encodeUtf8 $ T.unlines
+         [ "packages:"
+         , "- '.'"
+         , "explicit-setup-deps:"
+         , "  ! '*': true"
+         , "resolver: nightly-2016-12-05"
+         ]
+
+data Options = Options {
+      optProjectType :: ProjectType
+      }
+
+defaultOptions :: Options
+defaultOptions = Options {
+                   optProjectType = CabalNewBuild
+                 }
+
+optDescrs :: [OptDescr (Options -> Options)]
+optDescrs = [
+  Option "s" ["stack"] (NoArg $ \opt -> opt {optProjectType = Stack})
+    "generate a stack project"]
+
+showHelp :: String
+showHelp = concatMap optAsLine optDescrs
+  where optAsLine (Option flag (long:_) _ desc) =
+          "  -" ++ flag ++ "|--" ++ long ++ "\t" ++ desc ++ "\n"
+        optAsLine _ = error "showHelp"
+
 main :: IO ()
 main = do
-  dirs <- getArgs
+  args <- getArgs
+
+  let (actions, dirs, errors) = getOpt RequireOrder optDescrs args
+      options  = foldl (.) id actions defaultOptions
+
+  when ((not . null) errors) $ do
+         mapM_ (hPutStr stderr) errors
+         exitFailure
+
+  when (null dirs) $ do
+         hPutStrLn stderr "usage: genBuildInfo [options] dir1 [dir2 [...]]"
+         hPutStr stderr showHelp
+         exitFailure
+
   forM_ dirs $ \dir -> do
          info <- readGIRInfo (dir </> "pkg.info")
-         writeCabal (dir </> T.unpack (name info) <.> "cabal") info
+         writeCabal (optProjectType options)
+                    (dir </> T.unpack (name info) <.> "cabal") info
          writeSetup (dir </> "Setup.hs") info
          writeLicense (dir </> "LICENSE")
+         when (optProjectType options == Stack) $
+              writeStackYaml (dir </> "stack.yaml")
