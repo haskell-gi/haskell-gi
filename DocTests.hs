d6f068f906be4f5d17336a329fc3d9b660f763f0
Iñaki García Etxebarria <garetxe@gmail.com>
2017-01-03 12:36:38 +0100

    Fix doctest

--- DocTests.hs
+++ DocTests.hs
@@ -2,6 +2,7 @@ import Test.DocTest
 
 main :: IO ()
 main = doctest [ "-XCPP", "-XOverloadedStrings", "-XRankNTypes", "-XLambdaCase"
-               , "-idist/build/autogen"
-               , "lib/Data/GI/GIR"
-               , "lib/Data/GI/CodeGen" ]
+               , "-ilib"
+               , "Data.GI.GIR.GtkDoc"
+               , "Data.GI.CodeGen.ModulePath"
+               , "Data.GI.CodeGen.SymbolNaming" ]
