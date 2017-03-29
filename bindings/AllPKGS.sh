7e6d291e44afea57ec8e16b81a9f18ba0c588e45
Chris Lumens <clumens@redhat.com>
2017-03-29 10:13:26 -0400

    Add bindings for libostree.

--- bindings/AllPKGS.sh
+++ bindings/AllPKGS.sh
@@ -2,7 +2,7 @@
 unamestr=$(uname)
 
 # Packages compatible with base >= 4.7
-base47PKGS="GLib GObject Atk Gio Secret Soup cairo Pango PangoCairo GdkPixbuf Gdk Gtk GtkSource GIRepository Poppler JavaScriptCore-3.0 WebKit Vte Notify"
+base47PKGS="GLib GObject Atk Gio Secret Soup cairo Pango PangoCairo GdkPixbuf Gdk Gtk GtkSource GIRepository Poppler JavaScriptCore-3.0 WebKit Vte Notify OSTree"
 # Packages compatible with base >= 4.8
 base48PKGS="Gst GstBase GstAudio GstVideo"
 
