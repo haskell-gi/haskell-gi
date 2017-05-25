984d0ab227f509cdedd85c9924f79a1448af7eab
Arun Raghavan <arun@arunraghavan.net>
2017-05-25 14:53:05 +0530

    Add bindings for the GstTag-1.0 library

--- bindings/PKGS.sh
+++ bindings/PKGS.sh
@@ -5,7 +5,7 @@ unamestr=$(uname)
 # Packages compatible with base >= 4.7
 base47PKGS="GLib GObject Atk Gio Secret cairo Pango PangoCairo GdkPixbuf Gdk Gtk GtkSource Soup GIRepository Poppler JavaScriptCore-3.0 WebKit Vte Notify OSTree"
 # Packages compatible with base >= 4.8
-base48PKGS="Gst GstBase GstAudio GstVideo"
+base48PKGS="Gst GstBase GstAudio GstVideo GstTag"
 
 if [[ "$unamestr" == 'Darwin' ]]; then
     # Only present on OS X
