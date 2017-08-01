576a4781ef4c19fe13dc37b891f788e1cd121cc0
Brian C. Lane <bcl@redhat.com>
2017-08-01 15:26:40 -0700

    Add bindings for libgit2-glib
    This adds bindings for: https://git.gnome.org/browse/libgit2-glib/
    
    Import using:
    
    import GI.Ggit
    
    See the GNOME documentation at:
    https://developer.gnome.org/libgit2-glib/

--- bindings/PKGS.sh
+++ bindings/PKGS.sh
@@ -3,7 +3,7 @@ basever=$(ghc-pkg list base)
 unamestr=$(uname)
 
 # Packages compatible with base >= 4.7
-base47PKGS="GLib GObject Atk Gio Secret cairo Pango PangoCairo GdkPixbuf Gdk Gtk GtkSource Soup GIRepository Poppler JavaScriptCore-3.0 WebKit Vte Notify OSTree"
+base47PKGS="GLib GObject Atk Gio Secret cairo Pango PangoCairo GdkPixbuf Gdk Gtk GtkSource Soup GIRepository Poppler JavaScriptCore-3.0 WebKit Vte Notify OSTree Ggit"
 # Packages compatible with base >= 4.8
 base48PKGS="Gst GstBase GstAudio GstVideo GstTag GstPbutils"
 
