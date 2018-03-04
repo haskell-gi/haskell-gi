fc5d38d1dd122e0ca21fc6b78636b1edae65df43
Kilian Kilger <kkilger@gmail.com>
2018-03-04 20:13:00 +0100

    Fix intersect coding to be more elegant

--- src/Rectangle.hs
+++ src/Rectangle.hs
@@ -14,17 +14,17 @@ rBottomRightY :: Num a => Rectangle a -> a
 rBottomRightY r = rTopLeftY r + rHeight r 
 
 rIntersect :: (Ord a, Num a) => Rectangle a -> Rectangle a -> Maybe (Rectangle a)
-rIntersect r1 r2 = let insideBounds r1 r2 = rBottomRightX r1 >= rTopLeftX r2 && 
-                                            rBottomRightX r2 >= rTopLeftX r1 && 
-                                            rBottomRightY r1 >= rTopLeftY r2 && 
-                                            rBottomRightY r2 >= rTopLeftY r1   
+rIntersect r1 r2 = let notAbove r1 r2 =  rBottomRightX r1 >= rTopLeftX r2 && 
+                                         rBottomRightY r1 >= rTopLeftY r2  
+                       insideBounds r1 r2 = (&&) (notAbove r1 r2) (notAbove r2 r1)
+                       doIntersect r1 r2 = let topLeftX = max (rTopLeftX r1) (rTopLeftX r2)  
+                                               topLeftY = max (rTopLeftY r1) (rTopLeftY r2)     
+                                               btmRightX = min (rBottomRightX r1) (rBottomRightX r2)
+                                               btmRightY = min (rBottomRightY r1) (rBottomRightY r2) 
+                                           in Rectangle { rTopLeftX = topLeftX,
+                                                          rTopLeftY = topLeftY,
+                                                          rWidth = btmRightX - topLeftX,
+                                                          rHeight = btmRightY - topLeftY }   
                    in case insideBounds r1 r2 of 
-                      True -> let topLeftX = max (rTopLeftX r1) (rTopLeftX r2)  
-                                  topLeftY = max (rTopLeftY r1) (rTopLeftY r2)     
-                                  btmRightX = min (rBottomRightX r1) (rBottomRightX r2)
-                                  btmRightY = min (rBottomRightY r1) (rBottomRightY r2) 
-                              in Just Rectangle { rTopLeftX = topLeftX,
-                                                  rTopLeftY = topLeftY,
-                                                  rWidth = btmRightX - topLeftX,
-                                                  rHeight = btmRightY - topLeftY }
-                      False -> Nothing
+                        True -> Just $ doIntersect r1 r2 
+                        False -> Nothing
