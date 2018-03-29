c3d3d9e1b73115ae783ae06792ca109db29955d3
Murat Özsöyler <mozsoyler@adyazilim.com.tr>
2018-03-29 15:09:08 +0300

    examples/ folder rearranged and new introductory examples added.

---
+++ examples/introductory/src/basic.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE OverloadedStrings, OverloadedLabels #-}
+
+{- Basic example of GTK+ documentation. For information please refer to README -}
+
+module Main where
+  
+-- import Data.Int
+-- import qualified Data.Text as T
+import Data.Text ()
+-- import Debug.Trace (trace, traceIO)
+-- import System.Environment (getProgName, getArgs)
+
+import qualified GI.Gio as Gio
+import qualified GI.Gtk as Gtk
+import Data.GI.Base
+
+activateApp :: Gtk.Application -> IO ()
+activateApp app = do
+  w <- new Gtk.ApplicationWindow [ #application := app
+                                 , #title := "Haskell Gi - Examples - Basic" 
+                                 , #defaultWidth := 500
+                                 , #defaultHeight := 200
+                                 ]
+  #showAll w 
+  return ()
+
+main :: IO ()
+main = do
+  app <- new Gtk.Application [ #applicationId := "haskell-gi.examples.basic"
+                             , #flags := [ Gio.ApplicationFlagsFlagsNone ]
+                             ]
+  on app #activate $ do { activateApp app; return () }
+  Gio.applicationRun app Nothing
+  return ()
+
