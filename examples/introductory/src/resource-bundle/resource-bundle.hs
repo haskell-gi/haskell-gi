729440947a61301e4a670078daa2eb80381e07c9
Murat Özsöyler <mozsoyler@adyazilim.com.tr>
2018-04-15 19:48:52 +0300

    resource-bundle example added.

--- examples/introductory/src/resource-bundle/resource-bundle.hs
+++ examples/introductory/src/resource-bundle/resource-bundle.hs
@@ -13,27 +13,23 @@ import qualified GI.Gtk as Gtk
 unsafeGetObjectWithCast :: GObject o => Gtk.Builder -> Text -> (ManagedPtr o -> o) -> IO o
 unsafeGetObjectWithCast builder name totype_ = #getObject builder name >>= unsafeCastTo totype_ . fromJust
 
-resourceFileName :: FilePath -- [String] or Text tested
-resourceFileName = "C:/Users/zmtemp/Desktop/haskell/gtk/haskell-gi/examples/introductory/src/resource-bundle/resource-bundle.gresource"
+-- External resource bundles Step 2: Reactivate next 2 lines
+-- resourceFileName :: FilePath -- [String] or Text tested
+-- resourceFileName = "./resource-bundle.gresource"
 
 openAbout :: Gtk.AboutDialog -> IO ()
 openAbout aboutDialog = #run aboutDialog >> return ()
 
 appActivate :: Gtk.Application -> IO ()
 appActivate app = do
-  -- resource <- #load resourceFileName -- <== doesn't compile as no instance for load could be found
-  resource <- Gio.resourceLoad resourceFileName
-  Gio.resourcesRegister resource
+  -- External resource bundles Step 2: Reactivate next 2 lines
+  -- resource <- Gio.resourceLoad resourceFileName
+  -- Gio.resourcesRegister resource
 
-  -- builder <- #newFromResource "/haskell-gi/examples/resource-bundle/resource-bundle.ui" <== does not compile due to multiple instances of #newFromResource
   builder <- Gtk.builderNewFromResource "/haskell-gi/examples/resource-bundle/resource-bundle.ui"
   appWindow <- unsafeGetObjectWithCast builder "applicationwindow1" Gtk.ApplicationWindow
   #addWindow app appWindow
   aboutDialog <- unsafeGetObjectWithCast builder "aboutDialog" Gtk.AboutDialog
-  -- We manualy get the image for AboutDialog logo from the resource because GTK+ doesn't load it as per .gresource even though the logo file path is a resource:// url.
-  -- logo <- #newFromResource "/haskell-gi/examples/resource-bundle/Gnome-compressed-240x240.svg.png" <== does not compile due to multiple instances of #newFromResource
-  logo <- GdkPixbuf.pixbufNewFromResource "/haskell-gi/examples/resource-bundle/Gnome-compressed-240x240.svg.png"
-  set aboutDialog [#logo := logo]
   aboutWidget <- unsafeCastTo Gtk.Widget aboutDialog
   on aboutWidget #deleteEvent $ \_ -> #hideOnDelete aboutWidget
   mitem <- unsafeGetObjectWithCast builder "about" Gtk.MenuItem
