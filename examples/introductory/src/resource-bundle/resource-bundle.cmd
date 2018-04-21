b75737d279a0c72457564182d10c2a98f81e7712
Murat Özsöyler <mozsoyler@adyazilim.com.tr>
2018-04-21 19:24:13 +0300

    Final changes towards PR - Stop indication of image/ folder in resource-bundle.cmd - Prepend image/ folder to image paths in resource-bundle.gresource.xml and resource-bundle.ui. - Remove preprocess attribute form image definitons in resource-bundle.gresource.xml. - Bump to stack nightly-2018-04-21 resolver.

--- examples/introductory/src/resource-bundle/resource-bundle.cmd
+++ examples/introductory/src/resource-bundle/resource-bundle.cmd
@@ -1,5 +1,5 @@
 @rem resource bundle command for windows
-stack exec glib-compile-resources -- --sourcedir=. --sourcedir=./image --generate --target=resources.c .\resource-bundle.gresource.xml
+stack exec glib-compile-resources -- --sourcedir=. --generate --target=resources.c ./resource-bundle.gresource.xml
 
 @rem External resource bundles Step 1:
 @rem if you want a separate resource file from the executable the use below command instead of the one above
