5331c493ecefbd90769e142c8798e7e1ced845e8
Iñaki García Etxebarria <garetxe@protonmail.ch>
2019-06-03 20:49:37 +0100

    Generate bindings unconditionally
    We used to avoid generating bindings if the GI subdirectory was
    already present. This is problematic now that we distribute a set of
    pregenerated bindings (for documentation purposes), so generate the
    bindings unconditionally so the distributed ones get overwritten.

--- lib/Data/GI/CodeGen/CabalHooks.hs
+++ lib/Data/GI/CodeGen/CabalHooks.hs
@@ -21,17 +21,13 @@ import Data.GI.CodeGen.LibGIRepository (setupTypelibSearchPath)
 import Data.GI.CodeGen.ModulePath (toModulePath)
 import Data.GI.CodeGen.Overrides (parseOverridesFile, girFixups,
                                   filterAPIsAndDeps)
-import Data.GI.CodeGen.Util (ucFirst)
 
-import Control.Monad (when, void)
+import Control.Monad (void)
 
-import Data.Maybe (fromJust, fromMaybe)
+import Data.Maybe (fromJust)
 import qualified Data.Map as M
 import Data.Text (Text)
 import qualified Data.Text as T
-
-import System.Directory (doesFileExist)
-import System.FilePath ((</>), (<.>))
 
 type ConfHook = (GenericPackageDescription, HookedBuildInfo) -> ConfigFlags
               -> IO LocalBuildInfo
@@ -79,10 +75,7 @@ confCodeGenHook name version verbosity overrides outputDir
       cL' = ((fromJust . condLibrary) gpd) {condTreeData = ctd'}
       gpd' = gpd {condLibrary = Just cL'}
 
-  alreadyDone <- doesFileExist (fromMaybe "" outputDir
-                                </> "GI" </> T.unpack (ucFirst name) <.> "hs")
-  when (not alreadyDone) $ do
-    void $ writeModuleTree verbosity outputDir m
+  void $ writeModuleTree verbosity outputDir m
 
   lbi <- defaultConfHook (gpd', hbi) flags
 
