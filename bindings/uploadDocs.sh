ac4950477c50bd7641bde304020287c0b013c56d
Iñaki García Etxebarria <garetxe@protonmail.ch>
2019-06-16 14:36:55 +0100

    Upload docs for haskell-gi(-base) in uploadDocs
    [ci skip]

--- bindings/uploadDocs.sh
+++ bindings/uploadDocs.sh
@@ -6,8 +6,10 @@ cd ..
 
 DOCSDIR=dist-newstyle
 
-rm $DOCSDIR/*-docs.tar.gz
+rm -rf $DOCSDIR/*-docs.tar.gz
 cabal new-haddock --haddock-for-hackage all
+cabal upload --publish -d $DOCSDIR/haskell-gi-0.*-docs.tar.gz
+cabal upload --publish -d $DOCSDIR/haskell-gi-base-0.*-docs.tar.gz
 for doc in $DOCSDIR/gi-*-docs.tar.gz; do
     cabal upload --publish -d $doc
 done
