a479595fb3ad29cbf021f35ad850120e87dd90a7
Iñaki García Etxebarria <1238558+garetxe@users.noreply.github.com>
2019-07-23 16:15:34 +0100

    Separate out gtk4 packages from PKGS.sh
    They now live in GTK4-PKGS.sh
    
    This is because girepository can only load a single version of the
    library simultaneously, so
    
    cabal new-run genBuildInfo $(./PKGS.sh)
    
    would fail otherwise.

--- bindings/PKGS.sh
+++ bindings/PKGS.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-PKGS="GLib GObject Atk Gio Secret cairo Pango PangoCairo GdkPixbuf Gdk-3.0 Gtk-3.0 Graphene Gdk-4.0 Gsk Gtk-4.0 GtkSource Soup GIRepository Poppler Vte Notify OSTree Ggit xlib GdkX11 Dbusmenu DbusmenuGtk3 Wnck Handy Gst GstBase GstAudio GstVideo GstTag GstPbutils JavaScriptCore-4.0 WebKit2 WebKit2WebExtension"
+PKGS="GLib GObject Atk Gio Secret cairo Pango PangoCairo GdkPixbuf Gdk-3.0 Gtk-3.0 GtkSource Soup GIRepository Poppler Vte Notify OSTree Ggit xlib GdkX11 Dbusmenu DbusmenuGtk3 Wnck Handy Gst GstBase GstAudio GstVideo GstTag GstPbutils JavaScriptCore-4.0 WebKit2 WebKit2WebExtension"
 
 echo $PKGS
