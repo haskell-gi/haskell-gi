5113a80bd29206119dfa4a693437d17d80c0b4f0
Iñaki García Etxebarria <1238558+garetxe@users.noreply.github.com>
2019-08-04 10:28:02 +0100

    Do not try to build gtk4 bindings in Travis

--- travis/build.linux.sh
+++ travis/build.linux.sh
@@ -7,7 +7,7 @@ export HASKELL_GI_TYPELIB_SEARCH_PATH=/usr/lib/x86_64-linux-gnu/girepository-1.0
 
 # The version of Ubuntu in Travis does not have libhandy, libggit or libostree, and and its gtk version is ancient, so many things do not build anymore
 grep -Ev "(Handy|OSTree|haskell-gi-test|Ggit|webkit-example)" cabal.project > cabal.project.new; mv cabal.project.new cabal.project
-sed 's/Handy//;s/OSTree//;s/Ggit//' bindings/PKGS.sh > PKGS.new; mv PKGS.new bindings/PKGS.sh
+sed 's/Handy//;s/OSTree//;s/Ggit//;s/Graphene//;s/Gdk-4.0//;s/Gsk//;s/Gtk-4.0//' bindings/PKGS.sh > PKGS.new; mv PKGS.new bindings/PKGS.sh
 
 pushd bindings
 cabal new-run genBuildInfo $(bash PKGS.sh)
