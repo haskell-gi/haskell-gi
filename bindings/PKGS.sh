6298354e576099cb7546c226dc2f5a791267e27a
Sakamoto Noriaki <mrty.ityt.pt@gmail.com>
2020-05-29 07:14:39 +0900

    Add dependent package lists to pkg.info (#293)
    * Add packages field to pkg.info
    
    Now pkg.info file contains packages field, which is an object whose key
    is a distribution and the value is dependent package names.  Having an
    empty array as a value shows there is no dependent packages.
    
    * Disable Handy for Ubuntu
    
    libhandy-0.0-dev is available since 19.10.
    
    * Rename field name packages with distributionPackages

--- bindings/PKGS.sh
+++ bindings/PKGS.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
-PKGS="GLib GObject Atk Gio Secret cairo Pango PangoCairo GdkPixbuf Gdk-3.0 Gtk-3.0 Graphene Gdk-4.0 Gsk Gtk-4.0 GtkSource Soup GIRepository Poppler Vte Notify OSTree Ggit xlib GdkX11-3.0 GdkX11-4.0 Dbusmenu DbusmenuGtk3 Wnck Handy Gst GstBase GstAudio GstVideo GstTag GstPbutils JavaScriptCore-4.0 WebKit2 WebKit2WebExtension"
+show_help () {
+    echo "USAGE: $0 (list|deps) (fedora|ubuntu|ubuntu-ci)"
+}
 
-echo $PKGS
+DEFAULT_TARGET="fedora"
+
+BINDINGS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ACTION="${1:-list}"
+TARGET="${2:-$DEFAULT_TARGET}"
+
+case "$TARGET" in
+    fedora|ubuntu)
+        PKG_DEPS=".distributionPackages.$TARGET"
+        PKG_COND="$PKG_DEPS != null"
+        ;;
+    ubuntu-ci)
+        PKG_DEPS='.distributionPackages.ubuntu, .distributionPackages."ubuntu-ci"'
+        PKG_COND='.distributionPackages.ubuntu != null or .distributionPackages."ubuntu-ci" != null'
+        ;;
+    *)
+        echo "Unknown target"
+        show_help
+        exit 1
+esac
+
+case "$ACTION" in
+    list)
+        cd "$BINDINGS_DIR"
+        jq -r "select( $PKG_COND )"'| input_filename | split("/")[0]' */pkg.info 
+        ;;
+    deps)
+        cd "$BINDINGS_DIR"
+        jq -sr "[ .[] | ( $PKG_DEPS ) // [] ] | add | .[]" */pkg.info | sort | uniq
+        ;;
+    *)
+        echo "Unknown action"
+        show_help
+        exit 1
+esac
