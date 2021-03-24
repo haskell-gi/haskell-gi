3de308339b0d2aa61b404a16e1133eca13272a43
Matthias Heinzel <mheinzel@posteo.de>
2021-03-24 01:19:02 +0100

    make shebangs more portable
    Not all distros have bash in /bin/bash, potentially causing them to
    choke on the shebang.
    
    See also:
    https://stackoverflow.com/questions/10376206/what-is-the-preferred-bash-shebang

--- bindings/OSX-PKGS.sh
+++ bindings/OSX-PKGS.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 basever=$(ghc-pkg list base)
 unamestr=$(uname)
 
--- bindings/upload.sh
+++ bindings/upload.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 for sdist in sdists/*; do
     echo "Uploading $sdist"
